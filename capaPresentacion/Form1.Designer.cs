f146564fc79cc28fe4c697087ae6653115a84bea
ErikJC25 <160562444+ErikJC25@users.noreply.github.com>
2025-05-19 18:44:19 -0500

    .

--- capaPresentacion/Form1.Designer.cs
+++ capaPresentacion/Form1.Designer.cs
@@ -270,6 +270,7 @@
             this.Controls.Add(this.panel1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox11)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
