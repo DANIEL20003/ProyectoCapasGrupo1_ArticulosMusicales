6ad1a583d9b9ccd18383108a7ebdd19ac46e72a8
chuya369 <zhephyrus.stryker@gmail.com>
2025-05-19 03:07:54 -0500

    Form Buscar, Eliminar e iva
    Se agrego 3 formularios

---
+++ capaPresentacion/FRMBUSCARPRODUCTO.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace capaPresentacion
+{
+    partial class FRMBUSCARPRODUCTO
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.LBL_titulo = new System.Windows.Forms.Label();
+            this.LBLT_codigo = new System.Windows.Forms.Label();
+            this.TB_codigoProducto = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Indigo;
+            this.panel1.Controls.Add(this.LBL_titulo);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(799, 65);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.Indigo;
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel2.Location = new System.Drawing.Point(0, 421);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(799, 40);
+            this.panel2.TabIndex = 1;
+            // 
+            // LBL_titulo
+            // 
+            this.LBL_titulo.AutoSize = true;
+            this.LBL_titulo.Font = new System.Drawing.Font("Monotype Corsiva", 36F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LBL_titulo.ForeColor = System.Drawing.Color.Gold;
+            this.LBL_titulo.Location = new System.Drawing.Point(240, 4);
+            this.LBL_titulo.Name = "LBL_titulo";
+            this.LBL_titulo.Size = new System.Drawing.Size(319, 57);
+            this.LBL_titulo.TabIndex = 2;
+            this.LBL_titulo.Text = "Buscar Producto";
+            // 
+            // LBLT_codigo
+            // 
+            this.LBLT_codigo.AutoSize = true;
+            this.LBLT_codigo.Font = new System.Drawing.Font("Monotype Corsiva", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LBLT_codigo.ForeColor = System.Drawing.Color.Black;
+            this.LBLT_codigo.Location = new System.Drawing.Point(61, 104);
+            this.LBLT_codigo.Name = "LBLT_codigo";
+            this.LBLT_codigo.Size = new System.Drawing.Size(267, 25);
+            this.LBLT_codigo.TabIndex = 3;
+            this.LBLT_codigo.Text = "Ingrese el Código del producto:";
+            // 
+            // TB_codigoProducto
+            // 
+            this.TB_codigoProducto.Location = new System.Drawing.Point(334, 109);
+            this.TB_codigoProducto.Name = "TB_codigoProducto";
+            this.TB_codigoProducto.Size = new System.Drawing.Size(261, 20);
+            this.TB_codigoProducto.TabIndex = 4;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Indigo;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Monotype Corsiva", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.Gold;
+            this.button1.Location = new System.Drawing.Point(624, 104);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(113, 30);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Buscar";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Font = new System.Drawing.Font("Monotype Corsiva", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(33, 180);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(733, 220);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Producto";
+            // 
+            // FRMBUSCARPRODUCTO
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightGoldenrodYellow;
+            this.ClientSize = new System.Drawing.Size(799, 461);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.TB_codigoProducto);
+            this.Controls.Add(this.LBLT_codigo);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "FRMBUSCARPRODUCTO";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "FRMBUSCARPRODUCTO";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label LBL_titulo;
+        private System.Windows.Forms.Label LBLT_codigo;
+        private System.Windows.Forms.TextBox TB_codigoProducto;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.GroupBox groupBox1;
+    }
+}
