5c5f6a993e1ff8e0a1af6ca88821e330b42f13fa
ErikJC25 <160562444+ErikJC25@users.noreply.github.com>
2025-05-18 21:30:02 -0500

    Crea FRMMENUCLIENTE
    Se creo el formulario del menú principal para el cliente, y se lo conecto al FRMCLIENTE (Catálogo)

---
+++ capaPresentacion/FRMMENUCLIENTE.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace capaPresentacion
+{
+    partial class FRMMENUCLIENTE
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.pcb_carrito = new System.Windows.Forms.PictureBox();
+            this.pcb_catalogo = new System.Windows.Forms.PictureBox();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pcb_carrito)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pcb_catalogo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Indigo;
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(715, 89);
+            this.panel1.TabIndex = 0;
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.BackColor = System.Drawing.Color.Indigo;
+            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 377);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(715, 38);
+            this.flowLayoutPanel1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Monotype Corsiva", 36F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Gold;
+            this.label1.Location = new System.Drawing.Point(134, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(434, 57);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Bienvenido a Harmony";
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Monotype Corsiva", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label2.Location = new System.Drawing.Point(69, 284);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(255, 59);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Catálogo de instrumentos musicales";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("Monotype Corsiva", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label3.Location = new System.Drawing.Point(387, 284);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(244, 59);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Carrito de instrumentos musicales";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Monotype Corsiva", 14.25F, ((System.Drawing.FontStyle)(((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic) 
+                | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.DodgerBlue;
+            this.label4.Location = new System.Drawing.Point(643, 344);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(60, 22);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Ayuda";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // pcb_carrito
+            // 
+            this.pcb_carrito.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pcb_carrito.Image = global::capaPresentacion.Properties.Resources.Carrito_instrumentos;
+            this.pcb_carrito.Location = new System.Drawing.Point(415, 107);
+            this.pcb_carrito.Name = "pcb_carrito";
+            this.pcb_carrito.Size = new System.Drawing.Size(188, 174);
+            this.pcb_carrito.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pcb_carrito.TabIndex = 3;
+            this.pcb_carrito.TabStop = false;
+            // 
+            // pcb_catalogo
+            // 
+            this.pcb_catalogo.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pcb_catalogo.Image = global::capaPresentacion.Properties.Resources.Instrumentos;
+            this.pcb_catalogo.Location = new System.Drawing.Point(97, 107);
+            this.pcb_catalogo.Name = "pcb_catalogo";
+            this.pcb_catalogo.Size = new System.Drawing.Size(205, 174);
+            this.pcb_catalogo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pcb_catalogo.TabIndex = 2;
+            this.pcb_catalogo.TabStop = false;
+            this.pcb_catalogo.Click += new System.EventHandler(this.pcb_catalogo_Click);
+            // 
+            // FRMMENUCLIENTE
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightGoldenrodYellow;
+            this.ClientSize = new System.Drawing.Size(715, 415);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pcb_carrito);
+            this.Controls.Add(this.pcb_catalogo);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Controls.Add(this.panel1);
+            this.Name = "FRMMENUCLIENTE";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "FRMMENUCLIENTE";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pcb_carrito)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pcb_catalogo)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.PictureBox pcb_catalogo;
+        private System.Windows.Forms.PictureBox pcb_carrito;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
