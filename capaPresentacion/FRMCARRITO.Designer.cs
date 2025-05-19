7ce0f584b0ca3be5a299ee0b9220eeb91d71442f
ErikJC25 <160562444+ErikJC25@users.noreply.github.com>
2025-05-19 15:20:29 -0500

    Diseño del FRMCARRITO
    Se ajusto el diseño del formulario

--- capaPresentacion/FRMCARRITO.Designer.cs
+++ capaPresentacion/FRMCARRITO.Designer.cs
@@ -28,108 +28,121 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
-            this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
-            this.LBLpreciototal = new System.Windows.Forms.Label();
-            this.LBLivaaplicado = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.groupBox1.SuspendLayout();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.btn_volver = new System.Windows.Forms.Button();
+            this.lbl_precio_total = new System.Windows.Forms.Label();
+            this.lbl_iva = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
-            // 
-            // groupBox1
-            // 
-            this.groupBox1.BackColor = System.Drawing.Color.Indigo;
-            this.groupBox1.Controls.Add(this.label1);
-            this.groupBox1.Location = new System.Drawing.Point(0, -10);
-            this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(802, 95);
-            this.groupBox1.TabIndex = 0;
-            this.groupBox1.TabStop = false;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.ForeColor = System.Drawing.Color.Gold;
-            this.label1.Location = new System.Drawing.Point(148, 35);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(520, 31);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Instrumentos Musicales Seleccionados";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Font = new System.Drawing.Font("Monotype Corsiva", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.Location = new System.Drawing.Point(22, 114);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(537, 20);
+            this.label2.Size = new System.Drawing.Size(488, 22);
             this.label2.TabIndex = 1;
-            this.label2.Text = "Estos son los instrumentos musicales seleccionados previamente:\r\n";
+            this.label2.Text = "Estos son los instrumentos musicales guardados en tu carrito:";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(492, 159);
+            this.label3.Font = new System.Drawing.Font("Monotype Corsiva", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(517, 159);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(109, 20);
+            this.label3.Size = new System.Drawing.Size(112, 22);
             this.label3.TabIndex = 2;
             this.label3.Text = "Precio Total:";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(492, 202);
+            this.label4.Font = new System.Drawing.Font("Monotype Corsiva", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(517, 193);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(116, 20);
+            this.label4.Size = new System.Drawing.Size(120, 22);
             this.label4.TabIndex = 3;
             this.label4.Text = "IVA aplicado:";
-            // 
-            // LBLpreciototal
-            // 
-            this.LBLpreciototal.AutoSize = true;
-            this.LBLpreciototal.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.LBLpreciototal.Location = new System.Drawing.Point(611, 159);
-            this.LBLpreciototal.Name = "LBLpreciototal";
-            this.LBLpreciototal.Size = new System.Drawing.Size(0, 20);
-            this.LBLpreciototal.TabIndex = 4;
-            // 
-            // LBLivaaplicado
-            // 
-            this.LBLivaaplicado.AutoSize = true;
-            this.LBLivaaplicado.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.LBLivaaplicado.Location = new System.Drawing.Point(611, 202);
-            this.LBLivaaplicado.Name = "LBLivaaplicado";
-            this.LBLivaaplicado.Size = new System.Drawing.Size(0, 20);
-            this.LBLivaaplicado.TabIndex = 5;
-            // 
-            // button1
-            // 
-            this.button1.BackColor = System.Drawing.Color.Green;
-            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.ForeColor = System.Drawing.Color.White;
-            this.button1.Location = new System.Drawing.Point(496, 375);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(138, 40);
-            this.button1.TabIndex = 6;
-            this.button1.Text = "Volver";
-            this.button1.UseVisualStyleBackColor = false;
             // 
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView1.Location = new System.Drawing.Point(26, 159);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(460, 256);
+            this.dataGridView1.Size = new System.Drawing.Size(469, 232);
             this.dataGridView1.TabIndex = 7;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Indigo;
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(800, 100);
+            this.panel1.TabIndex = 8;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.Indigo;
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel2.Location = new System.Drawing.Point(0, 416);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(800, 34);
+            this.panel2.TabIndex = 9;
+            // 
+            // btn_volver
+            // 
+            this.btn_volver.BackColor = System.Drawing.Color.LimeGreen;
+            this.btn_volver.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_volver.Font = new System.Drawing.Font("Monotype Corsiva", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_volver.ForeColor = System.Drawing.Color.White;
+            this.btn_volver.Location = new System.Drawing.Point(521, 359);
+            this.btn_volver.Name = "btn_volver";
+            this.btn_volver.Size = new System.Drawing.Size(105, 32);
+            this.btn_volver.TabIndex = 26;
+            this.btn_volver.Text = "Volver";
+            this.btn_volver.UseVisualStyleBackColor = false;
+            // 
+            // lbl_precio_total
+            // 
+            this.lbl_precio_total.AutoSize = true;
+            this.lbl_precio_total.Font = new System.Drawing.Font("Monotype Corsiva", 14.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_precio_total.Location = new System.Drawing.Point(635, 159);
+            this.lbl_precio_total.Name = "lbl_precio_total";
+            this.lbl_precio_total.Size = new System.Drawing.Size(66, 22);
+            this.lbl_precio_total.TabIndex = 27;
+            this.lbl_precio_total.Text = "$ 234.25";
+            // 
+            // lbl_iva
+            // 
+            this.lbl_iva.AutoSize = true;
+            this.lbl_iva.Font = new System.Drawing.Font("Monotype Corsiva", 14.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_iva.Location = new System.Drawing.Point(643, 193);
+            this.lbl_iva.Name = "lbl_iva";
+            this.lbl_iva.Size = new System.Drawing.Size(43, 22);
+            this.lbl_iva.TabIndex = 28;
+            this.lbl_iva.Text = "15 %";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Monotype Corsiva", 36F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Gold;
+            this.label1.Location = new System.Drawing.Point(94, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(622, 57);
+            this.label1.TabIndex = 29;
+            this.label1.Text = "Carrito de instrumentos musicales";
             // 
             // FRMCARRITO
             // 
@@ -137,35 +150,36 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.LightGoldenrodYellow;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lbl_iva);
+            this.Controls.Add(this.lbl_precio_total);
+            this.Controls.Add(this.btn_volver);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
             this.Controls.Add(this.dataGridView1);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.LBLivaaplicado);
-            this.Controls.Add(this.LBLpreciototal);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.groupBox1);
             this.Name = "FRMCARRITO";
             this.Text = "FRMCARRITO";
-            this.groupBox1.ResumeLayout(false);
-            this.groupBox1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.GroupBox groupBox1;
-        private System.Windows.Forms.Label label1;
         private System.ComponentModel.BackgroundWorker backgroundWorker1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.Label LBLpreciototal;
-        private System.Windows.Forms.Label LBLivaaplicado;
-        private System.Windows.Forms.Button button1;
         private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button btn_volver;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lbl_precio_total;
+        private System.Windows.Forms.Label lbl_iva;
     }
 }
--- capaPresentacion/FRMCARRITO.cs
+++ capaPresentacion/FRMCARRITO.cs
@@ -16,5 +16,7 @@ namespace capaPresentacion
         {
             InitializeComponent();
         }
+
+        
     }
 }
