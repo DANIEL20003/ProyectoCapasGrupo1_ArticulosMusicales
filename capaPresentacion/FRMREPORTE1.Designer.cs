c9f4d9c343986c881d3844c20866c99fbc7fc518
Daniel Floresmilo Quiguiri Quezada <88177799+DANIEL20003@users.noreply.github.com>
2025-05-21 08:46:53 -0500

    hice un informe nomas ,pero me faltan tablas de la bd al parecer

--- capaPresentacion/FRMREPORTE1.Designer.cs
+++ capaPresentacion/FRMREPORTE1.Designer.cs
@@ -28,19 +28,58 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.bDPRACTICALBDataSet = new capaPresentacion.BDPRACTICALBDataSet();
+            this.bDPRACTICALBDataSetBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bDPRACTICALBDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bDPRACTICALBDataSetBindingSource)).BeginInit();
             this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToOrderColumns = true;
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.DataSource = this.bDPRACTICALBDataSetBindingSource;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 62;
+            this.dataGridView1.RowTemplate.Height = 28;
+            this.dataGridView1.Size = new System.Drawing.Size(1037, 559);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // bDPRACTICALBDataSet
+            // 
+            this.bDPRACTICALBDataSet.DataSetName = "BDPRACTICALBDataSet";
+            this.bDPRACTICALBDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // bDPRACTICALBDataSetBindingSource
+            // 
+            this.bDPRACTICALBDataSetBindingSource.DataSource = this.bDPRACTICALBDataSet;
+            this.bDPRACTICALBDataSetBindingSource.Position = 0;
             // 
             // FRMREPORTE1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1037, 559);
+            this.Controls.Add(this.dataGridView1);
             this.Name = "FRMREPORTE1";
             this.Text = "FRMREPORTE1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bDPRACTICALBDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bDPRACTICALBDataSetBindingSource)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.BindingSource bDPRACTICALBDataSetBindingSource;
+        private BDPRACTICALBDataSet bDPRACTICALBDataSet;
     }
 }
